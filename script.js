59f0337189d990a559657eb0ed49779c7b50b6f2
Ricardo Cano <ricardocanoc06@gmail.com>
2024-11-19 19:04:43 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById("forms").addEventListener("submit", function (event) {
+    event.preventDefault(); 
+  
+    const nombre = document.getElementById("nombre").value;
+    const pais = document.getElementById("pais").value;
+    const genero = document.querySelector('input[name="genero"]:checked')?.value;
+    const intereses = Array.from(document.querySelectorAll('input[name="intereses"]:checked')).map(el => el.value);
+  
+    const resultado = `
+      <p><strong>Nombre:</strong> ${nombre}</p>
+    
+      <p><strong>Género:</strong> ${genero}</p>
+      <p><strong>Intereses:</strong> ${intereses.join(", ") || "Ninguno"}</p>
+    `;
+  
+    document.getElementById("resultado").innerHTML = resultado;
+  });
+  document.getElementById("resetear").addEventListener("click", function () {
+    document.getElementById("formulario").reset();
+    document.getElementById("resultado").innerHTML = "";
+  });
+  
